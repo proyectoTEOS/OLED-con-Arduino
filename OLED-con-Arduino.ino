dd349c32107c1f9bea141d2eb350adfc77d21c47
TEOS <proyectoteos@gmail.com>
2018-06-24 21:37:25 -0500

    Code and diagram
    Program code and assembly scheme

---
+++ OLED-con-Arduino.ino
@@ -0,0 +1,37 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+  WEB https://goo.gl/jtiYJy
+*/
+
+#include <Wire.h>
+#include <Adafruit_SSD1306.h> //https://github.com/adafruit/Adafruit_SSD1306
+#include <Adafruit_GFX.h> //https://github.com/adafruit/Adafruit-GFX-Library
+
+Adafruit_SSD1306 display(-1);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+}
+
+void loop() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(33, 17);
+  display.setTextColor(BLACK, WHITE);
+  display.println("           ");
+  display.setCursor(33, 25);
+  display.println(" HOLA TEOS ");
+  display.setCursor(33, 33);
+  display.println("           ");
+  display.display();
+  delay(1000);
+}
+
